79d94599d19e912747e1f30da804c1fd5907e731
Norberto Rojas <norberto.rojas@softtek.com>
2021-08-30 08:07:44 -0300

    Desafio 03 primer commit

---
+++ ejercicios.js
@@ -0,0 +1,24 @@
+//Ejercicio 1
+
+const cal = function(){
+    let i = 12
+    let count = 0
+    while (i < 50) {
+        count += i 
+        console.log(i);
+        i += 2;        
+        
+    }
+    console.log(count);
+}
+
+cal();
+
+//Ejercicio 2
+//Realizar una función(n) que calcule la secuencia fibonacci de n elementos .(branch sugerida "/desafio02")
+
+
+
+//Ejercicio 3
+
+
